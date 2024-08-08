bcce224f18b2e14adc9e032044dec32194f8f4ab
nikolaskalvin <nikolaskalvin@gmail.com>
2024-08-08 22:29:51 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace program_functiondemo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+while (i < 5) 
+{
+Console.WriteLine(i);
+i++;
+}
+    }
+}
+}
